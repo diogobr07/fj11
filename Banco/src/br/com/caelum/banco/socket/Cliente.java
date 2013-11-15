470e8d573b09a7c2a43504c346f9261b5299ed1a
diogo <diogobruno07@gmail.com>
2013-11-15 11:51:02 -0200

    chat client

---
+++ Banco/src/br/com/caelum/banco/socket/Cliente.java
@@ -0,0 +1,48 @@
+package br.com.caelum.banco.socket;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Cliente {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		//dispare cliente
+		new Cliente("192.168.133.50", 30000).executa();
+
+	}
+	
+	private String host;
+	private int porta;
+	
+	public Cliente(String host, int porta){
+		this.host = host;
+		this.porta = porta;
+	}
+	
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente =  new Socket(this.host, this.porta);
+		System.out.println("O cliente se conectou ao servidor");
+		
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new Thread(r).start();
+		
+		//le mensagens do teclado e manda para o servidor
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida  = new PrintStream(cliente.getOutputStream());
+		while (teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
+		}
+		
+		saida.close();
+		teclado.close();
+		cliente.close();
+	}
+
+}
